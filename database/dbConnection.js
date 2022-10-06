63d8ac556d167affe0a79ff9d2d3cf5f7b117a2b
barnes-joseph <barnsjoseph05@gmail.com>
2022-10-06 06:43:27 +0000

    Fixed database connection errors

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,16 +1,21 @@
 require('dotenv').config();
 const {Client} = require('pg');
 
-const dbClient = new Client({
-    connectionString: process.env.DATABASE_URL,
+const dbClient = process.env.ENV === 'test' ? new Client({
+    connectionString: 'postgres://postgres:postgres@localhost:5432/postgres'
+
+}) : new Client({
+    connectionString: 'postgres://wmwooebafjunrs:19e66365d8654239103b4a680a9ee198df22c47f91242f0c0c3f6b01cf7f9883@ec2-44-206-214-233.compute-1.amazonaws.com:5432/de3l38ruet51gd',
     ssl: {
         rejectUnauthorized: false
       }
 })
 
+
 dbClient.connect().then(()=>{
     if(process.env.ENV !== 'test')
     console.log(`Database connected!`)
 });
+
 
 module.exports = dbClient;
